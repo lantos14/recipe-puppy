1c77c54925b91b9b2a52dadd2acb34a6e0b08466
lantost <lantos.tibor@kifu.gov.hu>
2019-05-15 18:17:30 +0200

    FIX: concat error on empty 2nd page

--- src/services/getRecipeData.js
+++ src/services/getRecipeData.js
@@ -1,16 +1,18 @@
-import fetchData from './fetchData';
-import concatLists from './concatLists';
+import fetchData from './api/getResponse';
+import concatLists from './utils/concatLists';
 
 const getRecipeData = async (input, pageNum) => {
   const result = [];
 
   for (let i = 1; i <= pageNum; i += 1) {
+    // need to use proxy due to CORS policy
     const url = `http://recipepuppyproxy.herokuapp.com/api/?q=${input}&p=${i}`;
     result.push(fetchData(url));
   }
 
   const lists = await Promise.all(result);
-
+  // there is no limit query in the API, so if there is more than 1 page result,
+  // I need to concatenate them for easier use of data
   return concatLists(lists);
 };
 
--- src/services/utils/concatLists.js
+++ src/services/utils/concatLists.js
@@ -1,6 +1,9 @@
 const concatLists = (lists) => {
   const result = [];
+
   lists.forEach((el) => {
+    if (!el) { return; }
+
     el.forEach((subel) => {
       result.push(subel);
     });
